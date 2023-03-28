b8f7639246cbe5e430e587ac090decc06f84b4c8
Michael Budde <mbu@jobindex.dk>
2023-03-28 16:59:26 +0200

    Fix subroutines in stacktraces

--- lib/Sentry/Stacktrace.pm
+++ lib/Sentry/Stacktrace.pm
@@ -18,8 +18,21 @@ sub prepare_frames ($self) {
 
   my @frames = reverse map { Sentry::Stacktrace::Frame->from_caller($_->@*) }
     $self->exception->frames->@*;
+  _shift_frame_subroutine(@frames);
 
   return [grep { $self->frame_filter->($_) } @frames];
+}
+
+# Shift the subroutine attribute one frame down. `caller` returns the
+# subroutine that the frame calls but Sentry expects the subroutine that the
+# calling frame called.
+sub _shift_frame_subroutine(@frames) {
+  my $caller_sub;
+  for my $frame (@frames) {
+    my $new_caller_sub = $frame->subroutine;
+    $frame->subroutine($caller_sub);
+    $caller_sub = $new_caller_sub;
+  }
 }
 
 sub TO_JSON ($self) {
